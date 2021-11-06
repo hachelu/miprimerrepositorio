a25c0f0e4402bbc2124c470f10b059350bc5688d
humberto <hachevazquez10@gmail.com>
2021-11-06 12:20:49 -0600

    agregue el segundo if con valor menor o igual a 19

--- imc.py
+++ imc.py
@@ -4,5 +4,7 @@ altura=float(input("ingresa tu altura en metros"))
 imc=masa/altura**2
 print("tu imc es:" +str(imc))
 
-if imc > 25:
+if imc >= 25:
       print("tiene sobrepeso")
+if imc <= 19:
+    print("tienes bajo peso")
