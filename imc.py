421d9cb51546516bf1aaf29566b743689d91cb99
humberto <hachevazquez10@gmail.com>
2021-11-06 12:17:32 -0600

    agrgue el primer if con valor mayor a 25

--- imc.py
+++ imc.py
@@ -2,3 +2,7 @@ masa=float(input("ingresa tu masa en kilogramos"))
 altura=float(input("ingresa tu altura en metros"))
 
 imc=masa/altura**2
+print("tu imc es:" +str(imc))
+
+if imc > 25:
+      print("tiene sobrepeso")
